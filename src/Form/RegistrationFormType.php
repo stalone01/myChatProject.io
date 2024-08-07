da8cf1d82cb38243e2fc9ec7126a96bd73b1338a
stalone01 <rasoamampioninastalone@gmail.com>
2024-08-07 17:41:31 +0300

    feat:registration and security

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username',TextType::class)
+            ->add('plainPassword',RepeatedType::class, [
+                'type'=>PasswordType::class,
+                'first_options'=>['label'=>'Password'],
+                'second_options'=>['label'=>'Repeat Password'],
+                'invalid_message'=>'The passwords fields must match',
+                'mapped'=>false,
+                'attr'=>['autocomplete'=>'new password']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
